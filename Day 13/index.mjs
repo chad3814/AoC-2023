ea5e511351d7aeb1df8f4345b1351826372b806f
Chad Walker <chad@cwalker.dev>
2023-12-17 21:11:16 -0600

    day 13 part 2

--- Day 13/index.mjs
+++ Day 13/index.mjs
@@ -17,6 +17,22 @@ async function parse() {
 // #####.##.
 // ..##..###
 // #....#..#`;
+//     const input =
+// `#.##..##.
+// ..#.##.#.
+// ##......#
+// ##......#
+// ..#.##.#.
+// ..##..##.
+// #.#.##.#.
+
+// #...##..#
+// #....#..#
+// ..##..###
+// #####.##.
+// #####.##.
+// ..##..###
+// #....#..#`;
     const lines = input.split('\n');
 
     const sections = [];
@@ -40,6 +56,14 @@ function isSame (a, b) {
     //     console.log(b.join(''));
     // }
     return same;
+}
+
+function isSameOffOne (a, b) {
+    let differences = 0;
+    for (let i = 0; i < a.length; i++) {
+        differences += (a[i] === b[i]) ? 0 : 1;
+    }
+    return differences === 1;
 }
 
 function getColum(section, column) {
@@ -69,7 +93,6 @@ function part1(sections) {
                     reflected = reflected && isSame(getColum(section, col - i - 1), getColum(section, col + i));
                 }
                 if (reflected) {
-                    console.log(`vertical with ${left} columns`);
                     // dumpSection(section);
                     total += left + 1;
                     hasVertical = true;
@@ -91,7 +114,6 @@ function part1(sections) {
                     reflected = reflected && isSame(section[row - i - 1], section[row + i]);
                 }
                 if (reflected) {
-                    console.log(`horizontal with ${above} rows`);
                     total += 100 * (above + 1);
                     hasHorizontal = true;
                     break;
@@ -107,7 +129,67 @@ function part1(sections) {
 }
 
 function part2(sections) {
+    let total = 0;
+    for (const section of sections) {
+        // check for vertical
+        let hasVertical = false;
+        let errored = false;
+        for (let col = 1; col < section[0].length; col++) {
+            errored = false;
+            const left = col - 1;
+            const right = section[0].length - col - 1;
+            const dist = Math.min(left, right);
+            let reflected = true;
+            for (let i = 0; i <= dist; i++) {
+                let same = isSame(getColum(section, col - i - 1), getColum(section, col + i));
+                if (!same && !errored) {
+                    same = isSameOffOne(getColum(section, col - i - 1), getColum(section, col + i));
+                    errored = same;
+                }
+                reflected = reflected && same;
+            }
+            if (reflected && errored) {
+                // dumpSection(section);
+                total += left + 1;
+                hasVertical = true;
+                console.log(`have a vertical reflection at ${left}`);
+                dumpSection(section);
+                break;
+            }
+        }
+        if (hasVertical) continue;
 
+        // check for horizontal
+        let hasHorizontal = false;
+        errored = false;
+        for (let row = 1; row < section.length; row++) {
+            errored = false;
+            const above = row - 1;
+            const below = section.length - row - 1;
+            const dist = Math.min(above, below);
+            let reflected = true;
+            for (let i = 0; i <= dist; i++) {
+                let same = isSame(section[row - i - 1], section[row + i]);
+                if (!same && !errored) {
+                    same = isSameOffOne(section[row - i - 1], section[row + i]);
+                    errored = same;
+                }
+                reflected = reflected && same;
+            }
+            if (reflected && errored) {
+                total += 100 * (above + 1);
+                hasHorizontal = true;
+                console.log(`have a horizontal reflect at ${above}`);
+                dumpSection(section);
+                break;
+            }
+        }
+        if (!hasHorizontal || !errored) {
+            dumpSection(section);
+            throw new Error('no reflection');
+        }
+    }
+    console.log(`total is ${total}`);
 }
 
 async function main() {
