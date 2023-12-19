afbdb511ee6ec302ee4c7f32bac7be4571a967d7
Chad Walker <chad@cwalker.dev>
2023-12-18 22:16:02 -0600

    day 12 part 2 still a no-go

--- Day 12/index.mjs
+++ Day 12/index.mjs
@@ -6,6 +6,7 @@ async function parse() {
     // const input = '???.### 1,1,3';
     // const input = '?###???????? 3,2,1';
     // const input = '.# 1';
+    // const input = '.?????.??#? 1,2';
     const lines = input.split('\n').filter(l => l.length > 0);
     const rows = [];
     for (const line of lines) {
@@ -68,12 +69,142 @@ function unfold(rows) {
     return unfolded;
 }
 
+function tokenize(springs) {
+    const tokens = [];
+    let token = '';
+    for (let i = 0; i < springs.length; i++) {
+        const c = springs[i];
+        if (c !== '.') {
+            token += c;
+            continue;
+        }
+        if (c !== '') {
+            tokens.push(token);
+            token = '';
+        }
+    }
+    if (token !== '') {
+        tokens.push(token);
+    }
+    return tokens;
+}
+
+function fits(map, start, size) {
+    const end = start + size - 1;
+    if (end >= map.length) return false; // can't be off the end
+    if (map[start -1] == '#') return false; // can't connect with previous
+    if (map[end + 1] == '#') return false; // can't connect with next
+    for (let i = start; i <= end; i++) {
+        if (map[i] == '.') return false; // has to be a string of springs
+    }
+    return true;
+}
+
+function findFirst(map, start, check) {
+    for (let i = start; i < map.length; i++) {
+        if (fits(map, i, check.size)) {
+            check.positions.push(i);
+            return i + check.size + 1;
+        }
+    }
+}
+function findLowest(map, checksum) {
+    let start = 0;
+    for (const check of checksum) {
+        start = findFirst(map, start, check);
+    }
+}
+function findLast(map, start, check) {
+    for (let i = start; i >= 0; i--) {
+        if (fits(map, i, check.size)) {
+            check.positions.push(i);
+            return i - check.size - 2;
+        }
+    }
+}
+function findHighest(map, checksum) {
+    let start = map.length - checksum[checksum.length - 1].size;
+    for (let i = checksum.length - 1; i >= 0; i--) {
+        start = findLast(map, start, checksum[i]);
+    }
+}
+function findMiddle(map, check) {
+    const start = check.positions[0] + 1;
+    const end = check.positions.pop();
+    for (let i = start; i < end; i++) {
+        if (fits(map, i, check.size)) {
+            check.positions.push(i);
+        }
+    }
+    check.positions.push(end);
+}
+function findAllMiddles(map, checksum) {
+    for (const check of checksum) {
+        findMiddle(map, check);
+    }
+}
+function count(map, checksum) {
+    for (let i = 0; i < checksum.length - 1; i++) {
+        const check = checksum[i];
+        const next = checksum[i + 1];
+        for (let p = 0; p < check.positions.length; p++) {
+            const position = check.positions[p];
+            const min = position + check.size + 1;
+            // first # possible in next check
+            const index = map.indexOf('#', min);
+            let max = index;
+            if (index === -1) max = map.length - 1;
+            for (let np = 0; np < next.positions.length; np++) {
+                const nextPos = next.positions[np];
+                if (nextPos >= min && nextPos <= max) next.counts[np] += check.counts[p];
+            }
+        }
+    }
+    const last = checksum.pop();
+    return last.counts.reduce((a, b) => a + b);
+}
 function part2(rows) {
-    const unfolded = unfold(rows);
-    // for (const row of unfolded) {
-    //     console.log(`${row.springs.join('')} ${row.checksum.join(',')}`);
-    // }
-    part1(unfolded);
+    rows = unfold(rows);
+    let sum = 0;
+    for (const row of rows) {
+        const map = tokenize(row.springs).join('.');
+        const checksum = [];
+        for (const size of row.checksum) {
+            checksum.push({size, positions:[]});
+        }
+        findLowest(map, checksum);
+        findHighest(map, checksum);
+        findAllMiddles(map, checksum);
+        console.log(`checksum: ${JSON.stringify(checksum)}`);
+        const first = map.indexOf('#');
+        if (first !== -1) {
+            const check = checksum[0];
+            for (let i = check.positions.length - 1; i >= 0; i--) {
+                // can't have the first check start after the first #
+                if (check.positions[i] > first) check.positions.pop();
+            }
+        }
+        const last = map.lastIndexOf('#');
+        if (last !== -1) {
+            const check = checksum.pop();
+            console.log(`last checksum ${check}`);
+            for (const position of check.positions) {
+                if ((position + check.size) < last) {
+                    // can't have the last check end before the last #
+                    check.positions.shift();
+                }
+            }
+            checksum.push(check);
+        }
+        console.log(`check 0 has positions ${checksum[0].positions.join(', ')}`);
+        checksum[0].counts = new Array(checksum[0].positions.length).fill(1);
+        for (let i = 1; i < checksum.length; i++) {
+            console.log(`check ${i} has positions ${checksum[i].positions.join(', ')}`);
+            checksum[i].counts = new Array(checksum[i].positions.length).fill(0);
+        }
+        sum += count(map, checksum);
+    }
+    console.log(`sum is ${sum}`);
 }
 
 async function main() {
